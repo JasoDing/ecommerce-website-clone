e5fc26e3270bf2af92d4ee2faa8a711e471cec67
yc791 <yc791@scarletmail.rutgers.edu>
2019-06-14 17:35:31 -0400

    working on the header.php

---
+++ header.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Header Page</title>
+
+    <link rel="stylesheet" type="text/css" href="./bootstrap.min.css"  />
+</head>
+<body>
+
+    <div class="card mb-3">
+        <ul class="nav nav-pills">
+            <li class="nav nav-item">
+                <img class="img" src="images/logo.jpg" width="100" height="60" alt="logo">
+            </li>
+            <li class="nav nav-item">
+                <form class="form-inline my-2 my-lg-0">
+                    <input class="form-control mr-sm-2" type="text" placeholder="Search">
+                    <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
+                </form>
+            </li>
+        </ul>
+
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <a class="navbar-brand" href="#">Navbar</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor03" aria-controls="navbarColor03" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarColor03">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Photography</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Computers</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Pro Video</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Lighting</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Pro Audio</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Mobile</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">TVs & Entertainment</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Camcorders</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Surveillance</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Optics</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Audio-Visual</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Index Page</title>
+
+    <link rel="stylesheet" type="text/css" href="./bootstrap.min.css"  />
+</head>
+<body>
+    <?php include('header.php'); ?>
+</body>
+</html>
