b32537fa603f96596836efe4d530200df9964414
cyy <chenyuyang0122@gmail.com>
2019-06-18 17:00:52 -0400

    add web service

---
+++ WebSite1/App_Code/WebService.vb
@@ -0,0 +1,36 @@
+﻿Imports System.Web
+Imports System.Web.Services
+Imports System.Web.Services.Protocols
+Imports System.Data
+Imports System.Configuration
+Imports System.Data.SqlClient
+
+' To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line.
+' <System.Web.Script.Services.ScriptService()> _
+
+<WebService(Namespace:="http://tempuri.org/")> _
+<WebServiceBinding(ConformsTo:=WsiProfiles.BasicProfile1_1)> _
+<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Public Class WebService
+    Inherits System.Web.Services.WebService
+
+
+    <WebMethod(Description:="get info")>
+    Public Function UserInfo() As DataTable
+        Dim constr As String = ConfigurationManager.ConnectionStrings("userLogin").ConnectionString
+        Using con As New SqlConnection(constr)
+            Using cmd As New SqlCommand("SELECT * FROM Users")
+                Using sda As New SqlDataAdapter()
+                    cmd.Connection = con
+                    sda.SelectCommand = cmd
+                    Using dt As New DataTable()
+                        dt.TableName = "Users"
+                        sda.Fill(dt)
+                        Return dt
+                    End Using
+                End Using
+            End Using
+        End Using
+    End Function
+
+End Class
---
+++ WebSite1/Default.aspx.vb
@@ -0,0 +1,18 @@
+﻿
+Partial Class _Default
+    Inherits System.Web.UI.Page
+
+
+    Protected Sub Page_Load(sender As Object, e As EventArgs) Handles Me.Load
+        If Not Me.IsPostBack Then
+            Me.BindGrid()
+        End If
+    End Sub
+
+    Private Sub BindGrid()
+        Dim service As New GetUserInfo.WebService()
+        GridView1.DataSource = service.UserInfo()
+        GridView1.DataBind()
+    End Sub
+
+End Class
