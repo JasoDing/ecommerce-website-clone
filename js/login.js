f2c40a51aef9bc8693a01457977452cb3b1ab057
yc791 <yc791@scarletmail.rutgers.edu>
2019-06-18 22:46:36 -0400

    working on the login page

--- js/login.js
+++ js/login.js
@@ -3,3 +3,19 @@ function getInfo(){
     var password = document.getElementById("password").value;
     console.log("Your username is: " + username + "and your password is: " + password);
 }
+
+function GetUserInfo(){
+    var request_url = '';
+
+    $.ajax({
+        url: request_url,
+        type: "POST",
+        dataType: "html",
+        success: function(response){
+            // Do something with your response here
+            alert(response);
+        }
+    });
+}
+
+console.log(GetUserInfo());
