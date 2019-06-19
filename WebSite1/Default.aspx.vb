5f02c3eba45842035ff7f7b5864cf80b37f50de3
cyy <chenyuyang0122@gmail.com>
2019-06-19 14:47:02 -0400

    update...

--- WebSite1/Default.aspx.vb
+++ WebSite1/Default.aspx.vb
@@ -1,16 +1,4 @@
 ﻿
 Partial Class _Default
     Inherits System.Web.UI.Page
-
-
-    Protected Sub Page_Load(sender As Object, e As EventArgs) Handles Me.Load
-        If Not Me.IsPostBack Then
-            Me.BindGrid()
-        End If
-    End Sub
-
-    Private Sub BindGrid()
-        Dim service As New GetUserInfo.WebService()
-    End Sub
-
 End Class
