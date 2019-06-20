b84ff5b097ce98b55404817889dd232e0cce432f
Vince <chenyuyang0122@gmail.com>
2019-06-20 10:15:49 -0400

    create another test.html to test ajax call

--- js/app.js
+++ js/app.js
@@ -245,19 +245,3 @@ document.addEventListener("DOMContentLoaded", ()=>{
         ui.cartLogic();
     })
 });
-
-
-
-
-function getUserProfile() {
-    $.ajax({
-        type: 'POST',
-        url: "http://localhost:54322/WebService.asmx",
-        data: {},
-        contentType: 'application/json; charset=utf-8',
-        dataType: 'json',
-        success: function (data) {alert(data.d);}
-    });
-}
-
-console.log(getUserProfile());
